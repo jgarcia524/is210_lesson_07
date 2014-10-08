72507880e9359f1652bfb842bb9cdb2e0eb63716
Joanne Garcia <joanne.garcia@spsmail.cuny.edu>
2014-10-08 17:30:25 -0400

    removed whitespace

--- task_04.py
+++ task_04.py
@@ -5,8 +5,8 @@
 import data
 
 data.BANDS['Buckingham Nicks'] = {
-    'Lindsey Buckingham' : ['guitar', 'vocals'],
-    'Stevie Nicks' : ['vocals', 'tambourine']
+    'Lindsey Buckingham': ['guitar', 'vocals'],
+    'Stevie Nicks': ['vocals', 'tambourine']
 }
 
 data.BANDS['Fleetwood Mac'].update(data.BANDS['Buckingham Nicks'])
