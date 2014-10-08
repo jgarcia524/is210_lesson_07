43141cda659ba705dde415fbd357f8ae074a0540
Joanne Garcia <joanne.garcia@spsmail.cuny.edu>
2014-10-08 17:30:10 -0400

    removed whitespace

--- task_03.py
+++ task_03.py
@@ -5,6 +5,6 @@
 import data
 
 CORRECTED = data.BANDS.copy()
-CORRECTED['Dylan'] = {'Bob Dylan' : ['vocals', 'guitar', 'harmonica']}
+CORRECTED['Dylan'] = {'Bob Dylan': ['vocals', 'guitar', 'harmonica']}
 del CORRECTED['Van Halen']['David Lee Roth']
 CORRECTED['Van Halen']['Sammy Hagar'] = ['vocals']
