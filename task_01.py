9ffdd48c717db6e02c326d18f22f8c69eeeaf6ad
Joanne Garcia <joanne.garcia@spsmail.cuny.edu>
2014-10-08 17:29:52 -0400

    removed whitespace

--- task_01.py
+++ task_01.py
@@ -3,22 +3,22 @@
 """Creating a dictionary"""
 
 GRADE_DATA = {
-    'Luke Skywalker' : {
-        'math' : 'B',
-        'etiquette' : 'B+',
-        'grammar' : 'B',
-        'gym' : 'A',
+    'Luke Skywalker': {
+        'math': 'B',
+        'etiquette': 'B+',
+        'grammar': 'B',
+        'gym': 'A',
     },
-    'Han Solo' : {
-        'math' : 'A-',
-        'etiquette' : 'C-',
-        'grammar' : 'B',
-        'gym' : 'B',
+    'Han Solo': {
+        'math': 'A-',
+        'etiquette': 'C-',
+        'grammar': 'B',
+        'gym': 'B',
     },
-    'C-3PO' : {
-        'math' : 'B',
-        'etiquette' : 'B+',
-        'grammar' : 'B',
-        'gym' : 'A',
+    'C-3PO': {
+        'math': 'B',
+        'etiquette': 'B+',
+        'grammar': 'B',
+        'gym': 'A',
     },
 }
