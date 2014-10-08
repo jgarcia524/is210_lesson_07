9c6b702465ec5e756906993b5530b110c17df665
Joanne Garcia <joanne.garcia@spsmail.cuny.edu>
2014-10-08 16:11:04 -0400

    task 1 complete

---
+++ task_01.py
@@ -0,0 +1,24 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Creating a dictionary"""
+
+GRADE_DATA = {
+    'Luke Skywalker' : {
+        'math' : 'B',
+        'etiquette' : 'B+',
+        'grammar' : 'B',
+        'gym' : 'A',
+    },
+    'Han Solo' : {
+        'math' : 'A-',
+        'etiquette' : 'C-',
+        'grammar' : 'B',
+        'gym' : 'B',
+    },
+    'C-3PO' : {
+        'math' : 'B',
+        'etiquette' : 'B+',
+        'grammar' : 'B',
+        'gym' : 'A',
+    },
+}
