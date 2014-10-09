d0a8e120d1047c54ecf06043bd10c0ec593b45e4
Joanne Garcia <joanne.garcia@spsmail.cuny.edu>
2014-10-09 06:25:47 -0400

    no where near finished - a little confused and ran out of time

---
+++ task_09.py
@@ -0,0 +1,20 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Merging Data Using a Dictionary Key."""
+
+import task_09_utility
+
+
+DATA_FILES = [
+    {'data': 'task_09_data/router_01.csv'},
+    {'data': 'task_09_data/router_02.csv'},
+    {'data': 'task_09_data/router_03.csv'}
+]
+
+def load_data(list_ob):
+    """DOCSTRING"""
+    counter = 0
+    {counter+=1 for }
+def
+
+#im lost
